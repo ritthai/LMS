f6b58cabdfe85908a1053e2dd3887662298a1958
Jonathan Koff <jonathankoff@gmail.com>
2010-05-08 17:49:54 -0400

    Removed some debugging output

--- search/youtube.php
+++ search/youtube.php
@@ -5,13 +5,10 @@ $TERMS = urlencode($terms);
 $TAGS = $tags;
 
 $sub = subsets($TAGS);
-print_r($sub);
 foreach($sub as $s) {
 	if(!count($sub)) continue;
 	$r = youtube_query($TERMS, $s);
-	//print_r($r);
 	if(count($r)) {
-		//print_r($_SESSION['youtube']);
 		$_SESSION['youtube'] = array_merge($_SESSION['youtube'], $r);
 		break;
 	}
