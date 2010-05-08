c13ce208a27365d76850c91d7615ec9692654f0a
Jonathan Koff <jonathankoff@gmail.com>
2010-05-08 10:17:16 -0400

    Made results session variable, added course save form

--- homepage.php
+++ homepage.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $PAGE_TITLE = "homepage.php";
 ?>
 <html>
@@ -8,13 +10,24 @@ Enter the search terms: <input type="text" id="terms" name="terms" />
 <input type="submit" value="Find resources" />
 </form>
 
+<hr/>
+
+<form action="<?php echo $PAGE_TITLE; ?>" method="post">
+Course name: <input type="text" id="course_name" name="course_name" />
+<input type="submit" value="Save" />
+</form>
+
 <?php
 if(isset($_POST['terms'])) {
 	$terms = $_POST['terms'];
 	echo "<hr/>";
-	eval("?>".file_get_contents("_google_search.php"));
+	eval("?>".file_get_contents("search/google.php"));
 	echo "<hr/>";
-	eval("?>".file_get_contents("_youtube_search.php"));
+	eval("?>".file_get_contents("search/youtube.php"));
+	
+} else if(isset($_SESSION['youtube']) && isset($_SESSION['google']) && isset($_POST['course_name'])) {
+	print($_POST['course_name']);
+	ourcei
 }
 ?>
 
---
+++ search/google.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+$TERMS = urlencode($terms." Lecture Notes");
+$USERIP = $_SERVER['REMOTE_ADDR'];
+
+$url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=$TERMS&userip=$USERIP&rsz=small";
+$data = file_get_contents($url);
+$arr = json_decode($data, true);
+$results = $arr['responseData']['results'];
+$store = array();
+?>
+<ul>
+<?php
+foreach($results as $elem) {
+	//print_r($elem);
+	$link = $elem['url'];
+	$title = $elem['titleNoFormatting'];
+	array_push($store, array($title, $link));
+?>
+<li><a href="<?php echo $link; ?>"><?php echo $title; ?></a></li>
+<?php
+}
+$_SESSION['google'] = $store;
+?>
+</ul>
