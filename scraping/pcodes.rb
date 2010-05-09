a2bd9dc9d99c858efa69b87a84f27c75672b6b1e
Jonathan Koff <jonathankoff@gmail.com>
2010-05-08 20:50:35 -0400

    Scraping code to get program codes

--- scraping/pcodes.rb
+++ scraping/pcodes.rb
@@ -3,5 +3,23 @@
 require 'net/http'
 http = Net::HTTP.new('ugradcalendar.uwaterloo.ca', 80)
 resp, data = http.get("/default.aspx?PageID=616", nil)
-data.match(/;Code=(...?.?.?.?.?)&amp;/m)
-print $1
+b = ""
+print "<body>"
+data.scan(/;Code=(...?.?.?.?.?)&amp;/m) { |a|
+	if !a[0].eql?(b)
+		print "<coursecode>"
+		print a[0]
+		print "</coursecode>\n"
+	end
+	b = a[0]
+	http2 = Net::HTTP.new('www.ucalendar.uwaterloo.ca', 80)
+	resp2, data2 = http2.get("/1011/COURSE/course-"+b+".html", nil)
+	print "<course>"
+	data2.scan(/<center><table border=0 width=80%><tr><td align=left><B><a name = "(.*){4,8}"><\/a>.*{0,30}<\/b><\/td><td align=right>Course ID: .*{4,8}<\/td><\/tr><tr><td colspan=2><b>(.*)<\/B><\/td><\/tr><tr><td colspan=2>(.*)<\/td>/m) { |c|
+		print "<code>"+c[0]+"</code>\n"
+		print "<title>"+c[1]+"</title>\n"
+		print "<description>"+c[2]+"</description>\n"
+	}
+	print "</course>\n\n"
+}
+print "</body>"
