a0c144e8eb0aea3dc44e17d1fffbb4946b2c12a9
Jonathan Koff <jonathankoff@gmail.com>
2010-05-09 13:15:51 -0400

    Scraper works for most courses

--- scraping/pcodes.rb
+++ scraping/pcodes.rb
@@ -6,6 +6,7 @@ resp, data = http.get("/default.aspx?PageID=616", nil)
 b = ""
 print "<body>"
 data.scan(/;Code=(...?.?.?.?.?)&amp;/m) { |a|
+	$stdout.flush
 	if !a[0].eql?(b)
 		print "<coursecode>"
 		print a[0]
@@ -15,7 +16,9 @@ data.scan(/;Code=(...?.?.?.?.?)&amp;/m) { |a|
 	http2 = Net::HTTP.new('www.ucalendar.uwaterloo.ca', 80)
 	resp2, data2 = http2.get("/1011/COURSE/course-"+b+".html", nil)
 	print "<course>"
-	data2.scan(/<center><table border=0 width=80%><tr><td align=left><B><a name = "(.*){4,8}"><\/a>.*{0,30}<\/b><\/td><td align=right>Course ID: .*{4,8}<\/td><\/tr><tr><td colspan=2><b>(.*)<\/B><\/td><\/tr><tr><td colspan=2>(.*)<\/td>/m) { |c|
+	$stdout.flush
+	data2.scan(/<center><table border=0 width=80%><tr><td align=left><B><a name = "(.{4,8})"><\/a>.{0,30}<\/b><\/td><td align=right>Course ID: .{4,8}<\/td><\/tr><tr><td colspan=2><b>(.{5,50})<\/B><\/td><\/tr><tr><td colspan=2>([^\[<]{10,500})<\/td>/m) { |c|
+		$stdout.flush
 		print "<code>"+c[0]+"</code>\n"
 		print "<title>"+c[1]+"</title>\n"
 		print "<description>"+c[2]+"</description>\n"
