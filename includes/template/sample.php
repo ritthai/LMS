e45477e8fab7c78ef6cd3a5992c38735873b5ebe
Jonathan Koff <jonathankoff@gmail.com>
2010-07-03 01:50:21 -0400

    Modified template sample.php file to remove JS Safari upload fix and add greeking

--- includes/template/sample.php
+++ includes/template/sample.php
@@ -8,24 +8,36 @@
 <?php include('template_begin.inc'); ?>
 <!-- /* ##### End of template_begin ##### */ -->
 
-		<script src="<?php echo "$HTMLROOT/js/prototype-1.6.0.3.js"; ?>" type="text/javascript"></script>
-		<script type="text/javascript">
-		/* From http://forums.macrumors.com/showthread.php?t=672301 :
-		 * A pretty little hack to make uploads not hang in Safari. Just call this
-		 * immediately before the upload is submitted. This does an Ajax call to
-		 * the server, which returns an empty document with the "Connection: close"
-		 * header, telling Safari to close the active connection. A hack, but
-		 * effective. */
-		function closeKeepAlive() {
-			if (/AppleWebKit|MSIE/.test(navigator.userAgent)) {
-				new Ajax.Request("/misc/empty.html", { asynchronous:false });
-			}
-		}
-		</script>
+
 
 <!-- /* ##### Beginning of template_notices ##### */ -->
 <?php include('template_notices.inc'); ?>
 <!-- /* ##### End of template_notices ##### */ -->
+
+<p>Blake: Let me have your attention for a moment! So you're talking about what? You're talking about...(puts out his cigarette)...bitching about that sale you shot, some son of a bitch that doesn't want to buy, somebody that doesn't want what you're selling, some broad you're trying to screw and so forth. Let's talk about something important. Are they all here?</p>
+Williamson: All but one.
+<p>Blake: Well, I'm going anyway. Let's talk about something important! (to Levene) Put that coffee down!! Coffee's for closers only. (Levene scoffs) Do you think I'm fucking with you? I am not fucking with you. I'm here from downtown. I'm here from Mitch and Murray. And I'm here on a mission of mercy. Your name's Levene?</p>
+Levene: Yeah.
+<p>Blake: You call yourself a salesman, you son of a bitch?</p>
+<p>Moss: I don't have to listen to this shit.</p>
+<p>Blake: You certainly don't pal. 'Cause the good news is -- you're fired. The bad news is you've got, all you got, just one week to regain your jobs, starting tonight. Starting with tonights sit. Oh, have I got your attention now? Good. 'Cause we're adding a little something to this months sales contest. As you all know, first prize is a Cadillac Eldorado. Anyone want to see second prize? Second prize's a set of steak knives. Third prize is you're fired. You get the picture? You're laughing now? You got leads. Mitch and Murray paid good money. Get their names to sell them! You can't close the leads you're given, you can't close shit, you ARE shit, hit the bricks pal and beat it 'cause you are going out!!!</p>
+Levene: The leads are weak.
+<p>Blake: 'The leads are weak.' Fucking leads are weak? You're weak. I've been in this business fifteen years.</p>
+<p>Moss: What's your name?</p>
+<p>Blake: FUCK YOU, that's my name!! You know why, Mister? 'Cause you drove a Hyundai to get here tonight, I drove a eighty thousand dollar BMW. That's my name!! (to Levene) And your name is "you're wanting." And you can't play in a man's game. You can't close them. (at a near whisper) And you go home and tell your wife your troubles. (to everyone again) Because only one thing counts in this life! Get them to sign on the line which is dotted! You hear me, you fucking faggots?</p>
+<p>(Blake flips over a blackboard which has two sets of letters on it: ABC, and AIDA.)</p>
+<p>Blake: A-B-C. A-always, B-be, C-closing. Always be closing! Always be closing!! A-I-D-A. Attention, interest, decision, action. Attention -- do I have your attention? Interest -- are you interested? I know you are because it's fuck or walk. You close or you hit the bricks! Decision -- have you made your decision for Christ?!! And action. A-I-D-A; get out there!! You got the prospects comin' in; you think they came in to get out of the rain? Guy doesn't walk on the lot unless he wants to buy. Sitting out there waiting to give you their money! Are you gonna take it? Are you man enough to take it? (to Moss) What's the problem pal? You. Moss.</p>
+<p>Moss: You're such a hero, you're so rich. Why you coming down here and waste your time on a bunch of bums?</p>
+<p>(Blake sits and takes off his gold watch)</p>
+<p>Blake: You see this watch? You see this watch?</p>
+<p>Moss: Yeah.</p>
+<p>Blake: That watch cost more than your car. I made $970,000 last year. How much you make? You see, pal, that's who I am. And you're nothing. Nice guy? I don't give a shit. Good father? Fuck you -- go home and play with your kids!! (to everyone) You wanna work here? Close!! (to Aaronow) You think this is abuse? You think this is abuse, you cocksucker? You can't take this -- how can you take the abuse you get on a sit?! You don't like it -- leave. I can go out there tonight with the materials you got, make myself fifteen thousand dollars! Tonight! In two hours! Can you? Can you? Go and do likewise! A-I-D-A!! Get mad! You sons of bitches! Get mad!! You know what it takes to sell real estate?</p>
+<p>(He pulls something out of his briefcase)</p>
+<p>Blake: It takes brass balls to sell real estate.</p>
+<p>(He's holding two brass balls on string, over the appropriate "area"--he puts them away after a pause)</p>
+<p>Blake: Go and do likewise, gents. The money's out there, you pick it up, it's yours. You don't--I have no sympathy for you. You wanna go out on those sits tonight and close, close, it's yours. If not you're going to be shining my shoes. Bunch of losers sitting around in a bar. (in a mocking weak voice) "Oh yeah, I used to be a salesman, it's a tough racket." (he takes out large stack of red index cards tied together with string from his briefcase) These are the new leads. These are the Glengarry leads. And to you, they're gold. And you don't get them. Because to give them to you is just throwing them away. (he hands the stack to Williamson) They're for closers.</p>
+<p>I'd wish you good luck but you wouldn't know what to do with it if you got it. (to Moss as he puts on his watch again) And to answer your question, pal: why am I here? I came here because Mitch and Murray asked me to, they asked me for a favor. I said, the real favor, follow my advice and fire your fucking ass because a loser is a loser.</p>
+<p>(He stares at Moss for a sec, and then picking up his briefcase, goes into inner office with Williamson)</p>
 
 <!-- /* ##### Beginning of template_end ##### */ -->
 <?php include('template_end.inc'); ?>
