a75f91d4b7a333f83bdf64427dc6bfa0c400de4b
Jonathan Koff <jonathankoff@gmail.com>
2010-07-11 23:53:41 -0400

    No more empty result lists for a data source

--- course/views/dataacquisition.view.php
+++ course/views/dataacquisition.view.php
@@ -1,17 +1,23 @@
 <?php foreach($args['searchresults'] as $subject) { ?>
+
 <div style="border: 1px solid #000">
 <ul>Subject: <?php echo $subject['subject']; ?>
+
+<?php	if($subject['google'] && count($subject['google'])) { ?>
 	<li><ul>Google results:
-<?php	foreach($subject['google'] as $res) {
-			$title = $res[0]; $link = $res[1];
+<?php		foreach($subject['google'] as $res) {
+				$title = $res[0]; $link = $res[1];
 ?>
 		<li><a href="<?php echo $link; ?>"><?php echo $title; ?></a></li>
-<?		} ?>
+<?php		} ?>
 	</ul></li>
+<?php	} ?>
+
+<?php	if($subject['youtube'] && count($subject['youtube'])) { ?>
 	<li>YouTube results:
 		<table>
-<?php	foreach($subject['youtube'] as $res) {
-			$width = 480/2; $height = 385/2;
+<?php		foreach($subject['youtube'] as $res) {
+				$width = 480/2; $height = 385/2;
 ?>
 		<tr>
 			<td><?php echo $res['title'] ? $res['title'] : '-'; ?></td>
@@ -31,11 +37,14 @@
 				</object>
 			</td>
 		</tr>
-<?		} ?>
+<?			} ?>
 	</table></li>
+<?php	} ?>
+
+<?php	if($subject['itunesu'] && count($subject['itunesu'])) { ?>
 	<li><ul>iTunesU results:
-<?php	foreach($subject['itunesu'] as $res) {
-			$imgwidth = $imgheight = 160;
+<?php		foreach($subject['itunesu'] as $res) {
+				$imgwidth = $imgheight = 160;
 ?>
 		<li>
 			<img	src="<?php echo $res['art']; ?>" style="float: left"
@@ -47,15 +56,22 @@
 		</li>
 <?		} ?>
 	</ul></li>
+<?php	} ?>
+
+<?php	if($subject['khanacad'] && count($subject['khanacad'])) { ?>
 	<li><ul>Khan Academy results:
-<?php	foreach($subject['khanacad'] as $res) { ?>
+<?php		foreach($subject['khanacad'] as $res) { ?>
 		<li>
 			<a href="<?php echo $res['url']; ?>">
 				<?php echo $res['title']; ?>
 			</a>
 		</li>
-<?		} ?>
+<?			} ?>
 	</ul></li>
+<?php	} ?>
+
 </ul>
 </div>
-<?php } ?>
+
+<?php } // for each subject ?>
+
