aac947687fba73e0bc638ebefabf3a39efc06414
Jonathan Koff <jonathankoff@gmail.com>
2010-09-18 00:31:41 -0400

    aLots of stuff

---
+++ includes/template/error.php
@@ -0,0 +1,18 @@
+<?php
+/**
+	Given:	...
+*/
+?>
+
+<?php include('template_begin.inc'); ?>
+	<style type="text/css">
+		body {
+			background-color: #655;
+			color: #eed;
+		}
+	</style>
+<?php include('template_notices.inc'); ?>
+
+<div id="loading_text"><p>Couldn't load the page, sorry!</p></div>
+
+<?php include('template_end.inc'); ?>
---
+++ includes/template/loading.php
@@ -0,0 +1,20 @@
+<?php
+/**
+	Given:	...
+*/
+?>
+
+<?php include('template_begin.inc'); ?>
+	<style type="text/css">
+		body {
+			background-color: #655;
+			color: #eed;
+		}
+	</style>
+	<link rel="stylesheet" href="/css/loading.css" text="text/css" media="screen,projection" />
+	<script type="text/javascript" src="/js/loading.js"> </script>
+<?php include('template_notices.inc'); ?>
+
+<div id="loading_text"><p>Loading...</p></div>
+
+<?php include('template_end.inc'); ?>
