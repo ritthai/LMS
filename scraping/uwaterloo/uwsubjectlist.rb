e728cfd6e9e4444ad4681a23d06aa26a3f308e03
Jonathan Koff <jonathankoff@gmail.com>
2010-10-16 18:09:17 -0400

    Fixed problem that caused missing subjects and missing closing parens in subjects

--- scraping/uwaterloo/uwsubjectlist.rb
+++ scraping/uwaterloo/uwsubjectlist.rb
@@ -6,9 +6,9 @@ require 'cgi'
 http = Net::HTTP.new('ugradcalendar.uwaterloo.ca', 80)
 resp, data = http.get("/page/Course-Descriptions-Index", nil)
 #data.scan(/<tr>\s*<td>(.{3,80})<\/td>\s*<td>(.{2,10})<\/td>(\s*<td> <\/td>\s*)*\s*<td><a shape="rect" href="\/courses.aspx?Code=([A-Z]{2-6})/m) { |a|
-data.scan(/<tr>\s*<td>([^<]{3,80})<\/td>.{10,280}Code=([A-Z]{2,8})&/m) { |a|
+data.scan(/<tr>\s*<td>([^<]{3,100})<\/td>.{10,280}Code=([A-Z]{2,8})(&|\")/m) { |a|
 	a[0].strip!()
 	a[0].gsub!("\'", "\\\\'")
-	a[0].gsub!(/\W*$/, '')
+	a[0].gsub!(/[^\w\)]*$/, '')
 	print "'#{a[1]}' => '#{a[0]}',\n"
 }
